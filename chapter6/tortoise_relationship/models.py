c709794202adbe895acc13ad7ea2d7f39e5dd3aa
parksunwoo <psw261@gmail.com>
2022-09-26 09:10:23 +0900

    add : chap6 mongo

--- chapter6/tortoise_relationship/models.py
+++ chapter6/tortoise_relationship/models.py
@@ -6,16 +6,6 @@ from tortoise.models import Model
 from tortoise import fields
 
 
-class CommentTortoise(Model):
-    id = fields.IntField(pk=True, generated=True)
-    post = fields.ForeignKeyField("models.PostTortoise", related_name="comments", null=False)
-    publication_date = fields.DatetimeField(null=False)
-    content = fields.TextField(null=False)
-
-    class Meta:
-        table = "comments"
-
-
 class CommentBase(BaseModel):
     post_id: int
     publication_date: datetime = Field(default_factory=datetime.now)
@@ -25,13 +15,8 @@ class CommentBase(BaseModel):
         orm_mode = True
 
 
-class CommentBase(BaseModel):
-    post_id: int
-    publication_date: datetime = Field(default_factory=datetime.now)
-    content: str
-
-    class Config:
-        orm_mode = True
+class CommentCreate(CommentBase):
+    pass
 
 
 class CommentDB(CommentBase):
@@ -47,6 +32,15 @@ class PostBase(BaseModel):
         orm_mode = True
 
 
+class PostPartialUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+
+class PostCreate(PostBase):
+    pass
+
+
 class PostDB(PostBase):
     id: int
 
@@ -58,6 +52,27 @@ class PostPublic(PostDB):
     def fetch_comments(cls, v):
         return list(v)
 
+
+class CommentTortoise(Model):
+    id = fields.IntField(pk=True, generated=True)
+    post = fields.ForeignKeyField(
+        "models.PostTortoise", related_name="comments", null=False
+    )
+    publication_date = fields.DatetimeField(null=False)
+    content = fields.TextField(null=False)
+
+    class Meta:
+        table = "comments"
+
+
+class PostTortoise(Model):
+    id = fields.IntField(pk=True, generated=True)
+    publication_date = fields.DatetimeField(null=False)
+    title = fields.CharField(max_length=255, null=False)
+    content = fields.TextField(null=False)
+
+    class Meta:
+        table = "posts"
 
 
 
